b68ef6bdaaee95bf9021ff6aac20ad6c83682c10
Pete Wilkins <pete.p.wilkins@gmail.com>
2017-04-30 13:28:16 +0100

    adds test/development databases

--- app/models/video.rb
+++ app/models/video.rb
@@ -11,6 +11,6 @@ class Video
 
 end
 
-DataMapper.setup(:default, "postgres://localhost/rekt_test")
+DataMapper.setup(:default, "postgres://localhost/rekt_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
